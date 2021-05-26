520dc91f6d7f0bb5d02a9a765ea8c200b4c219ef
Anderson Olliver <pla.olliver@gmail.com>
2021-05-25 23:39:22 -0400

    Adiciona WebAPI

---
+++ src/Dotnet.Teste.Api/Controllers/DataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Dotnet.Teste.Core.Entity;
+using Dotnet.Teste.Core.Repository;
+
+namespace Dotnet.Teste.Api.Controllers
+{
+    public class DataController : ApiController
+    {
+        private readonly OperationRepository _repository = new OperationRepository();
+
+        public HttpResponseMessage GetAll()
+        {
+            try
+            {
+
+                return Request.CreateResponse(HttpStatusCode.OK, _repository.list());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
+            }
+        }
+
+    }
+}
---
+++ src/Dotnet.Teste.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dotnet.Teste.Api.WebApiApplication" Language="C#" %>
