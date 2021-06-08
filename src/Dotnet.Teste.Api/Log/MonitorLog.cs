50d024043f77d26fc63bbc1f1515fe9a9ce4bc05
Anderson Olliver <pla.olliver@gmail.com>
2021-06-08 19:15:17 -0400

    Readiciona classe de log

---
+++ src/Dotnet.Teste.Api/Log/MonitorLog.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dotnet.Teste.Api.Log
+{
+    public class MonitorLog : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!request.RequestUri.PathAndQuery.Contains("api"))
+                return await base.SendAsync(request, cancellationToken);
+
+            var watcher = Stopwatch.StartNew();
+            var response = await base.SendAsync(request, cancellationToken);
+            watcher.Stop();
+            
+            Debug.Print($"[Log: {request.RequestUri.PathAndQuery}] - Tempo decorrido da requisição  {watcher.Elapsed}");
+            return response;
+
+        }
+    }
+}
